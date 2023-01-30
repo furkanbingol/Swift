e0f19bfc38d25cd84b2c99ebcd335fa1a1378255
Furkan Bingöl <furkanbingol176@gmail.com>
2023-01-30 22:25:09 +0300

    Project 04 added

---
+++ P04-MapJournal/P04-MapJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P04-MapJournal
+//
+//  Created by Furkan Bingöl on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
