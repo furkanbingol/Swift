4e66c1a62bc5a5c24a3b2bd25bf19128f42ada3e
Furkan Bingöl <furkanbingol176@gmail.com>
2023-02-12 23:28:57 +0300

    Autolayout&StackView added

---
+++ Autolayout&StackView/Autolayout&StackView/StackViewVC.swift
@@ -0,0 +1,20 @@
+//
+//  StackViewVC.swift
+//  Autolayout&StackView
+//
+//  Created by Furkan Bingöl on 12.02.2023.
+//
+
+import UIKit
+
+class StackViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //StackView konusunu öğrendim. Değişiklikler Storyboard dosyasında.
+    }
+    
+
+    
+}
---
+++ Autolayout&StackView/Autolayout&StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout&StackView
+//
+//  Created by Furkan Bingöl on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Layout konusunu detaylı öğrendim. Değişiklikler Storyboard dosyasında.
+    }
+
+
+}
+
