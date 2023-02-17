9268a0a117272962661252be2d371ee938558f7e
Furkan Bingöl <furkanbingol176@gmail.com>
2023-02-18 00:25:30 +0300

    AppLifecycle added

---
+++ AppLifecycle/AppLifecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifecycle
+//
+//  Created by Furkan Bingöl on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 'AppDelegate' ve 'SceneDelegate' ile Uygulama Anatomisini öğrendim.
+    }
+
+
+}
+
